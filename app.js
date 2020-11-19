0a8df26c1ef2d2878c75f72e82c6050237410fda
Vladimir <ryan_vancity_crawford@mail.ru>
2020-11-19 18:58:49 +0200

    checking for the rows and columns

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
 	const grid = document.querySelector('.grid')
 	const width = 8
 	const squares = []
+	let score = 0
 
 	const squareColors = [
 		'red',
@@ -93,6 +94,49 @@ document.addEventListener('DOMContentLoaded', () => {
 		} else squares[squareIdBeingDragged].style.backgroundColor = colorBeingDragged
 	}
 
+	//Одинаковые кубики
+	//линия из трех
+
+	function checkRowForThree() {
+		for (i = 0; i < 61; i++) {
+			let rowOfThree = [i, i+1, i+2]
+			let decidedColor = squares[i].style.backgroundColor
+			const isBlank = squares[i].style.backgroundColor === ''
+
+			if (rowOfThree.every(index => squares[index].style.backgroundColor === decidedColor && !isBlank)) {
+				score += 3
+				rowOfThree.forEach(index => {
+					squares[index].style.backgroundColor = ''
+				})
+			}
+		}
+	}
+
+	checkRowForThree()
+
+	//столбик из трех
+
+	function checkColumnForThree() {
+		for (i = 0; i < 47; i++) {
+			let columnOfThree = [i, i+width, i+width*2]
+			let decidedColor = squares[i].style.backgroundColor
+			const isBlank = squares[i].style.backgroundColor === ''
+
+			if (columnOfThree.every(index => squares[index].style.backgroundColor === decidedColor && !isBlank)) {
+				score += 3
+				columnOfThree.forEach(index => {
+					squares[index].style.backgroundColor = ''
+				})
+			}
+		}
+	}
+
+	checkColumnForThree()
+
+	window.setInterval(function(){
+		checkRowForThree()
+		checkColumnForThree()
+	}, 100)
 
 
 
