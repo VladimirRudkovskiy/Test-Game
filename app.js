0643ce1a6bf6ec082666ee656e43ea17584a7851
Vladimir <ryan_vancity_crawford@mail.ru>
2020-11-19 18:34:39 +0200

    drag squares

--- app.js
+++ app.js
@@ -17,6 +17,8 @@ document.addEventListener('DOMContentLoaded', () => {
 	function createBoard() {
 		for (let i = 0; i < width*width; i++) {
 			const square = document.createElement('div')
+			square.setAttribute('draggable', true)
+			square.setAttribute('id', i)
 			let randomColor = Math.floor(Math.random() * squareColors.length)
 			square.style.backgroundColor = squareColors[randomColor]
 			grid.appendChild(square)
@@ -26,5 +28,80 @@ document.addEventListener('DOMContentLoaded', () => {
 		}
 	}
 	createBoard()
+
+
+	//Перетаскивание кубиков
+
+	let colorBeingDragged
+	let colorBeingReplaced
+	let squareIdBeingDragged
+	let squareIdBeingReplaced
+
+	squares.forEach(square => square.addEventListener('dragstart', dragStart))
+	squares.forEach(square => square.addEventListener('dragend', dragEnd))
+	squares.forEach(square => square.addEventListener('dragover', dragOver))
+	squares.forEach(square => square.addEventListener('dragenter', dragEnter))
+	squares.forEach(square => square.addEventListener('dragleave', dragLeave))
+	squares.forEach(square => square.addEventListener('drop', dragDrop))
+
+	function dragStart() {
+		colorBeingDragged = this.style.backgroundColor
+		squareIdBeingDragged = parseInt(this.id)
+		console.log(colorBeingDragged)
+		console.log(this.id, 'dragstart')
+	}
+	
+	function dragOver(e) {
+		e.preventDefault()
+		console.log(this.id, 'dragover')
+	}
+
+	function dragEnter(e) {
+		e.preventDefault()
+		console.log(this.id, 'dragenter')
+	}
+
+	function dragLeave() {
+		console.log(this.id, 'dragleave')
+	}
+
+	function dragDrop() {
+		console.log(this.id, 'drop')
+		colorBeingReplaced = this.style.backgroundColor
+		squareIdBeingReplaced = parseInt(this.id)
+		this.style.backgroundColor = colorBeingDragged
+		squares[squareIdBeingDragged].style.backgroundColor = colorBeingReplaced
+
+	}
+
+	function dragEnd() {
+		console.log(this.id, 'dragend')
+		let validMoves = [
+			squareIdBeingDragged -1,
+			squareIdBeingDragged -width,
+			squareIdBeingDragged +1,
+			squareIdBeingDragged +width
+		]
+
+		let validMove = validMoves.includes(squareIdBeingReplaced)
+
+		if (squareIdBeingReplaced && validMove) {
+			squareIdBeingReplaced = null
+		} else if (squareIdBeingReplaced && !validMove) {
+			squares[squareIdBeingReplaced].style.backgroundColor = colorBeingReplaced
+			squares[squareIdBeingDragged].style.backgroundColor = colorBeingDragged
+		} else squares[squareIdBeingDragged].style.backgroundColor = colorBeingDragged
+	}
+
+
+
+
+
+
+
+
+
+
+
 
 })
